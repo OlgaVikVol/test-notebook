cd49dc8c6cea774ff416b24b16dc5a87b7692e47
Olga Volobueva <olvik1605@gmail.com>
2024-07-22 18:46:00 -0400

    add CardButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css';
 import Button from './components/Button/Button';
+import CardButton from './components/CardButton/CardButton';
+import JournalItem from './components/JournalItem/JournalItem';
 
 function App() {
-  return (
-    <>
-      <h1>Name</h1>
-      <Button />
-    </>
-  );
+	return (
+		<>
+			<h1>Name</h1>
+			<Button />
+			<CardButton>
+				<JournalItem />
+			</CardButton>
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/JournalItem/JournalItem.jsx
@@ -0,0 +1,15 @@
+import './JournalItem.css';
+
+function JournalItem() {
+	return (
+		<>
+			<h2 className="journal-item__header">1</h2>
+			<div className="journal-item__body">
+				<div className="journal-item__date">2</div>
+				<div className="journal-item__text">3</div>
+			</div>
+		</>
+	);
+}
+
+export default JournalItem;
